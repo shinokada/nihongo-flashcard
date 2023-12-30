03e929d08bd9bd168f63f4f5f5672db83dd2bedb
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2023-12-30 10:02:06 +0100

    style: add opacity to buttons

--- src/lib/FlashcardPage.svelte
+++ src/lib/FlashcardPage.svelte
@@ -11,8 +11,8 @@
 	let showCardBack = $state(false)	
 	let showFront = $state('Vis norsk')
 	let showBack = $state('Show English')
-  let japengClass = $state("text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800 opacity-100");
-	let engjapClass = $state("focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 opacity-50");
+  let lang1lang2 = $state("text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800 opacity-100");
+	let lang2lang1 = $state("focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 opacity-50");
 
 	const toggleShowBack = () => showCardBack = !showCardBack;
 
@@ -21,13 +21,13 @@
 		if (lang === 'japeng'){
 			showFront = '日本語'
 			showBack = 'English'
-			japengClass = twMerge(japengClass, 'opacity-100')
-			engjapClass = twMerge(engjapClass, 'opacity-50')
+			lang1lang2 = twMerge(lang1lang2, 'opacity-100')
+			lang2lang1 = twMerge(lang2lang1, 'opacity-50')
 		} else if (lang === 'engjap'){
       showFront = 'English'
 			showBack = '日本語'
-			japengClass = twMerge(japengClass, 'opacity-50')
-			engjapClass = twMerge(engjapClass, 'opacity-100')
+			lang1lang2 = twMerge(lang1lang2, 'opacity-50')
+			lang2lang1 = twMerge(lang2lang1, 'opacity-100')
 		}
 		showCardBack = false
     const { front: newFront, back: newBack } = getRandomPair(dictionary, lang);
@@ -62,8 +62,8 @@
 <div class="flex flex-col items-center mt-15">
 	<h1 class="text-3xl m-4">{title}</h1>
 	<div class="flex justify-between">
-		<button type="button" class="{japengClass}" on:click={() => updateLang('japeng')}>Japanese-English</button>
-		<button class="{engjapClass}" on:click={() => updateLang('engjap')}>English-Japanese</button>
+		<button type="button" class="{lang1lang2}" on:click={() => updateLang('japeng')}>Japanese-English</button>
+		<button class="{lang2lang1}" on:click={() => updateLang('engjap')}>English-Japanese</button>
 	</div>
 	<!-- FLASHCARD -->
 	<div class="bg-transparent w-full md:w-2/3 h-96">
