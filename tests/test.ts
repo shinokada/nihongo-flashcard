04be9a3e52c51d0e56663c23bf149ce5d7791c06
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-01-03 16:38:29 +0100

    test: add tests

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,30 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	expect(await page.textContent('h1')).toBe('単語 Words');
+});
+
+test('nouns page has expected h1', async ({ page }) => {
+	await page.goto('/nouns');
+	expect(await page.textContent('h1')).toBe('名詞 Nouns');
+});
+
+test('verbs page has expected h1', async ({ page }) => {
+	await page.goto('/verbs');
+	expect(await page.textContent('h1')).toBe('動詞 Verbs');
+});
+
+test('adjectives page has expected h1', async ({ page }) => {
+	await page.goto('/adjectives');
+	expect(await page.textContent('h1')).toBe('形容詞 Adjectives');
+});
+
+test('gairaigo page has expected h1', async ({ page }) => {
+	await page.goto('/gairaigo');
+	expect(await page.textContent('h1')).toBe('外来語 Gairaigo');
+});
+
+test('onomatopoeia page has expected h1', async ({ page }) => {
+	await page.goto('/onomatopoeia');
+	expect(await page.textContent('h1')).toBe('Onomatopoeia');
 });
