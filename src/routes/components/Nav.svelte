fcb6d23ee359613053d3f312d1f74e7ac52944ff
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-02-18 16:28:42 +0100

    docs: Nav update adding px-8

--- src/routes/components/Nav.svelte
+++ src/routes/components/Nav.svelte
@@ -19,11 +19,11 @@
 <header
     class="sticky top-0 z-40 mx-auto w-full flex-none border-b border-gray-200 bg-gray-100 dark:border-gray-600 dark:bg-gray-900"
   >
-  <Navbar {toggleNav} {closeNav} {navStatus} breakPoint="lg" fluid>
+  <Navbar {toggleNav} {closeNav} {navStatus} breakPoint="lg" fluid div2class="px-8">
 
 	{#snippet brand()}
 		<NavBrand siteName="Nihongo Flashcard" >
-			<Jp size="40" class="inline" />
+			<Jp size="40" class="inline"/>
 		</NavBrand>
 		<div class="flex items-center ml-auto lg:order-1">
             <Darkmode btnclass="inline-block dark:hover:text-white hover:text-gray-900" />
