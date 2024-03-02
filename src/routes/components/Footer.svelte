8c5a4d14587f37793c83f1579b81ff079e91527f
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-03-02 20:15:59 +0100

    fix: remove target="_blank"

--- src/routes/components/Footer.svelte
+++ src/routes/components/Footer.svelte
@@ -8,19 +8,19 @@
 		<FooterUl
 			ulclass="flex flex-wrap items-center mt-3 sm:pl-4 text-sm text-gray-500 dark:text-gray-400 gap-2 sm:mt-0"
 		>
-			<FooterLi href="https://multilanguage-flashcard.codewithshin.com/" target="_blank"
+			<FooterLi href="https://multilanguage-flashcard.codewithshin.com/" 
 				>Multilanguage flashcard</FooterLi
 			>
-			<FooterLi href="https://norske-flashcard.codewithshin.com/" target="_blank"
+			<FooterLi href="https://norske-flashcard.codewithshin.com/" 
 				>Norske flashcard</FooterLi
 			>
-			<FooterLi href="https://nihongo-flashcard.codewithshin.com/" target="_blank"
+			<FooterLi href="https://nihongo-flashcard.codewithshin.com/" 
 				>Japanese flashcard</FooterLi
 			>
-			<FooterLi href="https://flexilexi-dictionary.codewithshin.com/" target="_blank"
+			<FooterLi href="https://flexilexi-dictionary.codewithshin.com/" 
 				>FlexiLexi dictionary</FooterLi
 			>
-			<FooterLi href="https://gjett-norsk.codewithshin.com/" target="_blank">Gjett norsk</FooterLi>
+			<FooterLi href="https://gjett-norsk.codewithshin.com/" >Gjett norsk</FooterLi>
 		</FooterUl>
 	</div>
 </Footer>
