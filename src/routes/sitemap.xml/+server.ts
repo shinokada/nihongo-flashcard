e98caf00c5e97ee53c298d6247b894a01c67aa96
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-09-23 19:44:39 +0200

    site: add sitemap

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,9 @@
+// /src/routes/sitemap.xml/+server.ts
+import * as sitemap from 'super-sitemap';
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async () => {
+  return await sitemap.response({
+    origin: 'ttps://nihongo-flashcard.codewithshin.com',
+  });
+};
